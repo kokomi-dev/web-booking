beff4061d2abaf8d2ade2d903fc54d243846b67e
Nguyễn Thế An <nguyenthean12062002@gmail.com>
2024-11-18 22:23:30 +0700

    Upload: content page

--- src/app/(route)/(pages)/content/page.tsx
+++ src/app/(route)/(pages)/content/page.tsx
@@ -3,7 +3,7 @@ import SidebarContent from "@/components/dashboard/content/sidebar";
 import ShowContent from "@/components/dashboard/content/show-content";
 const ContentPage = () => {
   return (
-    <div className="grid grid-cols-[20%,80%] gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-[20%,80%] gap-4">
       <SidebarContent />
       <div>
         <ShowContent />
