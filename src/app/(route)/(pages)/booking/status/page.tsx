6c8a596417fa0f5106265aad6940ea5a44b7d73f
Nguyễn Thế An <nguyenthean12062002@gmail.com>
2025-02-24 15:12:49 +0700

    Update: download ticket pdf, keep connect server

--- src/app/(route)/(pages)/booking/status/page.tsx
+++ src/app/(route)/(pages)/booking/status/page.tsx
@@ -5,9 +5,13 @@ import { Button } from "@/components/ui/button";
 import QUERY_KEY_BOOKING from "@/services/queryKeyStore/bookingQueyKeyStore";
 import { IHotelRoom } from "@/types/hotel.type";
 import { useQuery } from "@tanstack/react-query";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import {
   Calendar,
   CheckCircle,
+  Download,
+  Home,
   Mail,
   MapPin,
   Ticket,
@@ -15,7 +19,7 @@ import {
   XCircle,
 } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Fragment } from "react";
+import { Fragment, useRef } from "react";
 
 const BookingSuccess = () => {
   const router = useRouter();
@@ -28,139 +32,184 @@ const BookingSuccess = () => {
       return res?.status === 200 ? res.data.data : {};
     },
     enabled: !!category && !!apptransid,
-    retry: 3,
-    retryDelay: 1000,
+    retry: 4,
+    retryDelay: 1200,
   });
   if (isLoading) {
     return <Loading />;
   }
+  const ticketRef = useRef<any>(null);
+
   const checkCategory = category === "attraction" ? true : false;
+  const handleDownloadPDF = async () => {
+    const ticketElement = document.getElementById("ticket");
+    if (!ticketElement) return;
+
+    const canvas = await html2canvas(ticketRef.current, {
+      scale: 3,
+      useCORS: true,
+    });
+
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF("p", "mm", "a4");
+    const imgWidth = 210;
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+    pdf.save(
+      `Thông tin ${category === "attraction" ? "vé" : "đặt phòng"} - ${
+        category === "attraction"
+          ? dataBooked?.infoAttraction?.name
+          : dataBooked?.infoHotel?.name
+      } `
+    );
+  };
   return (
     <Fragment>
       {Number(status) == 1 ? (
-        <div className="posing-vertical-2 flex items-center flex-col min-h-screen text-center p-6 bg-bg_primary_white rounded-14">
-          <CheckCircle className="text-green-600 w-20 h-20 mb-4" />
-          <h1 className="text-3xl font-bold text-green-700">
-            {checkCategory
-              ? "Đặt vé tham quan thành công!"
-              : "Đặt nơi lưu trú thành công !"}
-          </h1>
-          <p className="text-gray-700 mt-2">
-            Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi.
-          </p>
-          <p className="text-black mt-2">
-            Chúng tôi sẽ gửi phản hồi một lần nữa vào email của bạn, vui lòng
-            chú ý theo dõi!
-          </p>
+        <div className="w-full posing-vertical-2  flex items-center flex-col min-h-screen text-center p-2 md:p-4 lg:p-6 bg-bg_primary_white rounded-14">
+          <div
+            ref={ticketRef}
+            id="ticket"
+            className="posing-vertical-2 w-full  flex items-center flex-col text-center"
+          >
+            <CheckCircle className="text-green-600 w-20 h-20 mb-4" />
+            <h1 className="text-3xl font-bold text-green-700">
+              {checkCategory
+                ? "Đặt vé tham quan thành công!"
+                : "Đặt nơi lưu trú thành công !"}
+            </h1>
+            <p className="text-gray-700 mt-2">
+              Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi.
+            </p>
+            <p className="text-black mt-2">
+              Chúng tôi sẽ gửi phản hồi một lần nữa vào email của bạn, vui lòng
+              chú ý theo dõi!
+            </p>
 
-          {/* Card thông tin đặt vé */}
-          <div className="w-full mt-6 bg-white shadow-lg rounded-lg p-6 ">
-            <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
-              Thông tin đơn đặt
-            </h2>
-            <div className="space-y-2 text-left text-black_main">
-              <p className="flex items-center">
-                <Calendar className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
-                <b>Ngày đặt:</b>{" "}
-                {new Date(dataBooked?.bookedDate).toLocaleString("vi-VN")}
-              </p>
-              {category === "attraction" && (
+            {/* Card thông tin đặt vé */}
+            <div className="w-full mt-6 bg-white shadow-lg rounded-lg p-3 md:p-4 lg:p-6 ">
+              <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
+                Thông tin đơn đặt
+              </h2>
+              <div className="space-y-2 text-left text-black_main">
                 <p className="flex items-center">
                   <Calendar className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
-                  <b>Ngày khởi hành:</b>{" "}
-                  {new Date(dataBooked?.dateStart).toLocaleDateString("vi-VN")}
+                  <b>Ngày đặt:</b>{" "}
+                  {new Date(dataBooked?.bookedDate).toLocaleString("vi-VN")}
                 </p>
-              )}
-              {checkCategory ? (
-                <p className="flex items-center">
-                  <Ticket className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
-                  <b>Vé:</b> {dataBooked?.numberOfTicketsBooked.adult} người
-                  lớn, {dataBooked?.numberOfTicketsBooked.children} trẻ em
-                </p>
-              ) : (
-                <p className="flex items-center">
-                  <Ticket className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
-                  <b>Số lượng người:</b>{" "}
-                  {dataBooked?.numberOfTicketsBooked.adult} người lớn,{" "}
-                  {dataBooked?.numberOfTicketsBooked.children} trẻ em
-                </p>
-              )}
-              <p className="flex items-center text-blue_main_sub">
-                <b>Tổng tiền:</b> {dataBooked?.totalBooked.toLocaleString()} VND
-              </p>
-              <p className="flex items-center">
-                <b>Phương thức thanh toán:</b> {dataBooked?.paymentMethod}
-              </p>
-            </div>
-          </div>
-
-          {/* Card thông tin người đặt */}
-          <div className="w-full flex flex-col posing-vertical-3 lg:space-y-0 lg:space-x-4 lg:flex-row items-stretch lg:justify-between ">
-            <div className="h-full bg-white shadow-lg rounded-lg p-6 w-full flex-grow ">
-              <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
-                Thông tin khách hàng
-              </h2>
-              <p className="flex items-center">
-                <User className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
-                <b>Mã đơn:</b> {dataBooked?._id}
-              </p>
-              <p className="flex items-center text-blue_main_sub">
-                <Mail className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
-                <b>Email:</b> {dataBooked?.infoUser?.email}
-              </p>
-            </div>
-            {/* Card thông tin tour */}
-            {checkCategory ? (
-              <div className="h-full bg-white shadow-lg rounded-lg p-6 w-full flex-grow ">
-                <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
-                  Thông tin tour
-                </h2>
-                <p className="flex items-center">
-                  <b>Tên tour:</b> {dataBooked?.infoAttraction?.name}
-                </p>
-                <p className="flex items-center">
-                  <MapPin className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
-                  <b>Địa điểm:</b> {dataBooked?.infoAttraction?.address}
-                </p>
-              </div>
-            ) : (
-              <div className="h-full bg-white shadow-lg rounded-lg p-6 w-full flex-row ">
-                <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
-                  Thông tin lưu trú
-                </h2>
-                <p className="flex items-center">
-                  <b>Tên nơi lưu trú: </b> {dataBooked?.infoHotel?.name}
-                </p>
-                <p className="flex items-center">
-                  <b>Địa điểm: </b> {dataBooked?.infoHotel?.address}
-                </p>
-                {dataBooked.infoHotelRoom.map(
-                  (room: IHotelRoom, index: number) => {
-                    return (
-                      <div key={index}>
-                        <p className="flex items-center">
-                          <b>Tên phòng: </b> {room.name}
-                        </p>
-                        <p className="flex items-center">
-                          <b>Số phòng đã đặt: </b> {room.numberBooked}
-                        </p>
-                      </div>
-                    );
-                  }
+                {category === "attraction" && (
+                  <p className="flex items-center">
+                    <Calendar className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
+                    <b>Ngày khởi hành:</b>{" "}
+                    {new Date(dataBooked?.dateStart).toLocaleDateString(
+                      "vi-VN"
+                    )}
+                  </p>
                 )}
+                {checkCategory ? (
+                  <p className="flex items-center">
+                    <Ticket className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
+                    <b>Vé:</b> {dataBooked?.numberOfTicketsBooked.adult} người
+                    lớn, {dataBooked?.numberOfTicketsBooked.children} trẻ em
+                  </p>
+                ) : (
+                  <p className="flex items-center">
+                    <Ticket className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
+                    <b>Số lượng người:</b>{" "}
+                    {dataBooked?.numberOfTicketsBooked.adult} người lớn,{" "}
+                    {dataBooked?.numberOfTicketsBooked.children} trẻ em
+                  </p>
+                )}
+                <p className="flex items-center text-blue_main_sub">
+                  <b>Tổng tiền:</b> {dataBooked?.totalBooked.toLocaleString()}{" "}
+                  VND
+                </p>
+                <p className="flex items-center">
+                  <b>Phương thức thanh toán:</b> {dataBooked?.paymentMethod}
+                </p>
               </div>
-            )}
+            </div>
+            {/* Card thông tin người đặt */}
+            <div className="w-full flex flex-col posing-vertical-3 lg:space-y-0 lg:space-x-4 lg:flex-row items-stretch lg:justify-between ">
+              <div className="h-full bg-white shadow-lg rounded-lg p-3 md:p-4 lg:p-6 w-full flex-grow ">
+                <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
+                  Thông tin khách hàng
+                </h2>
+                <p className="flex items-center">
+                  <User className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
+                  <b>Mã đơn:</b> {dataBooked?._id}
+                </p>
+                <p className="flex items-center text-blue_main_sub">
+                  <Mail className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
+                  <b>Email:</b> {dataBooked?.infoUser?.email}
+                </p>
+              </div>
+              {/* Card thông tin tour */}
+              {checkCategory ? (
+                <div className="h-full bg-white shadow-lg rounded-lg p-3 md:p-4 lg:p-6 w-full flex-grow ">
+                  <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
+                    Thông tin tour
+                  </h2>
+                  <p className="flex items-center">
+                    <b>Tên tour:</b> {dataBooked?.infoAttraction?.name}
+                  </p>
+                  <p className="flex items-center">
+                    <MapPin className="w-5 h-5 mr-2 flex-shrink-0 text-black_sub" />{" "}
+                    <b>Địa điểm:</b> {dataBooked?.infoAttraction?.address}
+                  </p>
+                </div>
+              ) : (
+                <div className="h-full bg-white shadow-lg rounded-lg p-3 md:p-4 lg:p-6 w-full flex-row ">
+                  <h2 className="text-xl font-semibold mb-4 text-blue_main_sub">
+                    Thông tin lưu trú
+                  </h2>
+                  <p className="flex items-center">
+                    <b>Tên nơi lưu trú: </b> {dataBooked?.infoHotel?.name}
+                  </p>
+                  <p className="flex items-center">
+                    <b>Địa điểm: </b> {dataBooked?.infoHotel?.address}
+                  </p>
+                  {dataBooked.infoHotelRoom.map(
+                    (room: IHotelRoom, index: number) => {
+                      return (
+                        <div key={index}>
+                          <p className="flex items-center">
+                            <b>Tên phòng: </b> {room.name}
+                          </p>
+                          <p className="flex items-center">
+                            <b>Số phòng đã đặt: </b> {room.numberBooked}
+                          </p>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+              )}
+            </div>
           </div>
           {/* Nút điều hướng */}
-          <Button
-            className="bg-bg_primary_main text-white hover:bg-bg_primary_active w-full text-center max-w-lg"
-            onClick={() => router.push("/")}
-          >
-            Quay lại Trang Chủ
-          </Button>
+
+          <div className="w-full flex flex-col gap-y-2 lg:gap-y-0 lg:flex-row items-center justify-start lg:gap-x-2">
+            <Button
+              className="bg-bg_primary_main text-white hover:bg-bg_primary_active flex items-center justify-center gap-x-1 w-full text-center max-w-lg"
+              onClick={() => handleDownloadPDF()}
+            >
+              <Download />
+              Tải thông tin vé
+            </Button>
+            <Button
+              className="bg-white text-black_main hover:bg-bg_primary_hover w-full text-center max-w-lg flex items-center justify-center gap-x-1"
+              onClick={() => router.push("/")}
+            >
+              <Home />
+              Quay lại Trang Chủ
+            </Button>
+          </div>
         </div>
       ) : (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-bg_primary_white rounded-14 text-center p-6 posing-vertical-3">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-bg_primary_white rounded-14 text-center p-3 md:p-4 lg:p-6 posing-vertical-3">
           <XCircle className="text-red-600 w-20 h-20 " />
           <h1 className="text-large font-bold text-red-600">
             Đặt thất bại | Hoặc xảy ra lỗi giao dịch thanh toán trực tuyến
@@ -180,10 +229,12 @@ const BookingSuccess = () => {
               <strong>Hotline: </strong>0972345744
             </a>
           </div>
+
           <Button
-            className="bg-bg_primary_main text-white hover:bg-bg_primary_active "
+            className="bg-bg_primary_main text-white hover:bg-bg_primary_active flex items-center justify-start gap-x-1"
             onClick={() => router.push("/")}
           >
+            <Home />
             Quay lại Trang Chủ
           </Button>
         </div>
--- src/services/connect-server/keep-connect-server.ts
+++ src/services/connect-server/keep-connect-server.ts
@@ -1,41 +1,24 @@
 "use client";
 
 import { useEffect } from "react";
-
-let idleTimeout: NodeJS.Timeout | null = null;
 const apiUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
 export default function PingServer() {
   useEffect(() => {
-    document.addEventListener("mousemove", resetIdleTimer);
-    document.addEventListener("keydown", resetIdleTimer);
-    document.addEventListener("scroll", resetIdleTimer);
-    idleTimeout = setTimeout(keepServerAwake, 14.5 * 60 * 1000);
-
-    return () => {
-      document.removeEventListener("mousemove", resetIdleTimer);
-      document.removeEventListener("keydown", resetIdleTimer);
-      document.removeEventListener("scroll", resetIdleTimer);
-
-      if (idleTimeout) clearTimeout(idleTimeout);
-    };
+    keepServerAwake();
+    const interval = setInterval(keepServerAwake, 14 * 60 * 1000);
+    return () => clearInterval(interval);
   }, []);
 
   return null;
 }
-
-function resetIdleTimer() {
-  if (idleTimeout) clearTimeout(idleTimeout);
-  idleTimeout = setTimeout(keepServerAwake, 14 * 60 * 1000);
-}
-
 function keepServerAwake() {
   fetch(`${apiUrl}/attraction/keep-server-live`)
     .then((response) => {
-      if (response) {
-        console.log("Server is awake!");
+      if (response.ok) {
+        console.log("Server is live!");
       } else {
-        console.error("Server response error:", response);
+        console.error("Server response error:", response.status);
       }
     })
     .catch((error) => console.error("Error pinging server:", error));
